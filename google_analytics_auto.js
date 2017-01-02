906e044aad543183ad69d8f229c16047bd3b8276
giangbimin <gianghoang.dev@gmail.com>
2017-01-02 11:41:27 +0700

    iniziate

---
+++ google_analytics_auto.js
@@ -0,0 +1,234 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css">
+	<title>Page not found &#8211; Templates PRO</title>
+<link rel='dns-prefetch' href='//fonts.googleapis.com' />
+<link rel='dns-prefetch' href='//s.w.org' />
+<link rel="alternate" type="application/rss+xml" title="Templates PRO &raquo; Feed" href="http://www.templatespro.com.br/feed/" />
+<link rel="alternate" type="application/rss+xml" title="Templates PRO &raquo; Comments Feed" href="http://www.templatespro.com.br/comments/feed/" />
+		<script type="text/javascript">
+			window._wpemojiSettings = {"baseUrl":"https:\/\/s.w.org\/images\/core\/emoji\/2.2.1\/72x72\/","ext":".png","svgUrl":"https:\/\/s.w.org\/images\/core\/emoji\/2.2.1\/svg\/","svgExt":".svg","source":{"concatemoji":"http:\/\/www.templatespro.com.br\/wp-includes\/js\/wp-emoji-release.min.js?ver=44d3a00f90623f88e9a1706f7fc201f6"}};
+			!function(a,b,c){function d(a){var b,c,d,e,f=String.fromCharCode;if(!k||!k.fillText)return!1;switch(k.clearRect(0,0,j.width,j.height),k.textBaseline="top",k.font="600 32px Arial",a){case"flag":return k.fillText(f(55356,56826,55356,56819),0,0),!(j.toDataURL().length<3e3)&&(k.clearRect(0,0,j.width,j.height),k.fillText(f(55356,57331,65039,8205,55356,57096),0,0),b=j.toDataURL(),k.clearRect(0,0,j.width,j.height),k.fillText(f(55356,57331,55356,57096),0,0),c=j.toDataURL(),b!==c);case"emoji4":return k.fillText(f(55357,56425,55356,57341,8205,55357,56507),0,0),d=j.toDataURL(),k.clearRect(0,0,j.width,j.height),k.fillText(f(55357,56425,55356,57341,55357,56507),0,0),e=j.toDataURL(),d!==e}return!1}function e(a){var c=b.createElement("script");c.src=a,c.defer=c.type="text/javascript",b.getElementsByTagName("head")[0].appendChild(c)}var f,g,h,i,j=b.createElement("canvas"),k=j.getContext&&j.getContext("2d");for(i=Array("flag","emoji4"),c.supports={everything:!0,everythingExceptFlag:!0},h=0;h<i.length;h++)c.supports[i[h]]=d(i[h]),c.supports.everything=c.supports.everything&&c.supports[i[h]],"flag"!==i[h]&&(c.supports.everythingExceptFlag=c.supports.everythingExceptFlag&&c.supports[i[h]]);c.supports.everythingExceptFlag=c.supports.everythingExceptFlag&&!c.supports.flag,c.DOMReady=!1,c.readyCallback=function(){c.DOMReady=!0},c.supports.everything||(g=function(){c.readyCallback()},b.addEventListener?(b.addEventListener("DOMContentLoaded",g,!1),a.addEventListener("load",g,!1)):(a.attachEvent("onload",g),b.attachEvent("onreadystatechange",function(){"complete"===b.readyState&&c.readyCallback()})),f=c.source||{},f.concatemoji?e(f.concatemoji):f.wpemoji&&f.twemoji&&(e(f.twemoji),e(f.wpemoji)))}(window,document,window._wpemojiSettings);
+		</script>
+		<style type="text/css">
+img.wp-smiley,
+img.emoji {
+	display: inline !important;
+	border: none !important;
+	box-shadow: none !important;
+	height: 1em !important;
+	width: 1em !important;
+	margin: 0 .07em !important;
+	vertical-align: -0.1em !important;
+	background: none !important;
+	padding: 0 !important;
+}
+</style>
+			<style>
+			#wp-admin-bar-layers-edit-layout .ab-icon:before{
+				font-family: "layers-interface" !important;
+				content: "\e62f" !important;
+				font-size: 16px !important;
+			}
+			</style>
+		<link rel='stylesheet' id='wpmenucart-icons-css'  href='http://www.templatespro.com.br/wp-content/plugins/woocommerce-menu-bar-cart/css/wpmenucart-icons.css?ver=44d3a00f90623f88e9a1706f7fc201f6' type='text/css' media='all' />
+<link rel='stylesheet' id='wpmenucart-css'  href='http://www.templatespro.com.br/wp-content/plugins/woocommerce-menu-bar-cart/css/wpmenucart-main.css?ver=44d3a00f90623f88e9a1706f7fc201f6' type='text/css' media='all' />
+<link rel='stylesheet' id='layers-google-fonts-css'  href='//fonts.googleapis.com/css?family=Mandali%3Aregular&#038;ver=1.5.1' type='text/css' media='all' />
+<link rel='stylesheet' id='layers-framework-css'  href='http://www.templatespro.com.br/wp-content/themes/layerswp/assets/css/framework.css?ver=1.5.1' type='text/css' media='all' />
+<link rel='stylesheet' id='layers-components-css'  href='http://www.templatespro.com.br/wp-content/themes/layerswp/assets/css/components.css?ver=1.5.1' type='text/css' media='all' />
+<link rel='stylesheet' id='layers-responsive-css'  href='http://www.templatespro.com.br/wp-content/themes/layerswp/assets/css/responsive.css?ver=1.5.1' type='text/css' media='all' />
+<link rel='stylesheet' id='layers-icon-fonts-css'  href='http://www.templatespro.com.br/wp-content/themes/layerswp/assets/css/layers-icons.css?ver=1.5.1' type='text/css' media='all' />
+<link rel='stylesheet' id='layers-woocommerce-css'  href='http://www.templatespro.com.br/wp-content/themes/layerswp/assets/css/woocommerce.css?ver=1.5.1' type='text/css' media='all' />
+<link rel='stylesheet' id='layers-style-css'  href='http://www.templatespro.com.br/wp-content/themes/templatespro/style.css?ver=1.5.1' type='text/css' media='all' />
+			<script>
+				(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+				(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+				m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+				})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+				ga('create', 'CUSTOM', 'auto');
+				ga('send', 'pageview');
+			</script>
+		<script type='text/javascript' src='http://www.templatespro.com.br/wp-includes/js/jquery/jquery.js?ver=1.12.4'></script>
+<script type='text/javascript' src='http://www.templatespro.com.br/wp-includes/js/jquery/jquery-migrate.min.js?ver=1.4.1'></script>
+<script type='text/javascript' src='http://www.templatespro.com.br/wp-content/themes/layerswp/assets/js/plugins.js?ver=1.5.1'></script>
+<link rel='https://api.w.org/' href='http://www.templatespro.com.br/wp-json/' />
+<link rel="EditURI" type="application/rsd+xml" title="RSD" href="http://www.templatespro.com.br/xmlrpc.php?rsd" />
+<link rel="wlwmanifest" type="application/wlwmanifest+xml" href="http://www.templatespro.com.br/wp-includes/wlwmanifest.xml" /> 
+
+		<!-- Custom Logo: hide header text -->
+		<style id="custom-logo-css" type="text/css">
+			.sitetitle, .tagline {
+				position: absolute;
+				clip: rect(1px, 1px, 1px, 1px);
+			}
+		</style>
+	<style type="text/css" id="layers-inline-styles-header">
+ h1,h2,h3,h4,h5,h6, .heading { font-family: "Mandali", Helvetica, sans-serif;} 
+ button, .button, input[type=submit] { font-family: "Mandali", Helvetica, sans-serif;} 
+ .header-site, .header-site.header-sticky { background-color: rgba(255, 255, 255, 1); } 
+ .title-container { background-color: #ffffff; } 
+  input[type="button"],    input[type="submit"],    button,    .button,    .form-submit input[type="submit"],    .invert input[type="button"],    .invert input[type="submit"],    .invert button,    .invert .button,    .invert .form-submit input[type="submit"]{ background-color: #4c7942; color: #FFFFFF; } 
+  input[type="button"]:hover,    input[type="submit"]:hover,    button:hover,    .button:hover,    .form-submit input[type="submit"]:hover,    .invert input[type="button"]:hover,    .invert input[type="submit"]:hover,    .invert button:hover,    .invert .button:hover,    .invert .form-submit input[type="submit"]:hover{ background-color: #82a17b; color: #FFFFFF; } 
+.copy a:not(.button),  .story a:not(.button){ color: #4c7942;border-bottom-color: #4c7942;} 
+.copy a:not(.button):hover,  .story a:not(.button):hover{ color: #82a17b;border-bottom-color: #82a17b;} 
+ .footer-site { background-color: #4c7942; } </style>					<meta property="og:title" content=" &raquo; Page not found" />
+			<meta property="og:type" content="website" />
+			<meta property="og:url" content="" />
+							<meta property="og:image" content="http://www.templatespro.com.br/wp-content/uploads/2015/05/logo_p.png" />
+					<script src='/google_analytics_auto.js'></script></head>
+<body class="error404 wp-custom-logo layers-header-sticky layers-header-overlay">
+	<div class="wrapper invert off-canvas-right" id="off-canvas-right">
+    <a class="close-canvas" data-toggle="#off-canvas-right" data-toggle-class="open">
+        <i class="l-close"></i>
+        Close    </a>
+
+    <div class="content nav-mobile clearfix">
+        <nav class="nav nav-vertical"><ul id="menu-menu-top" class="menu"><li id="menu-item-25" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-home menu-item-25"><a href="http://www.templatespro.com.br/">Home</a></li>
+<li id="menu-item-29" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-29"><a href="http://www.templatespro.com.br/templates/">Templates</a></li>
+<li id="menu-item-100" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-100"><a href="http://www.templatespro.com.br/contact/">Contact</a></li>
+<li id="menu-item-88" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-88"><a href="http://www.templatespro.com.br/my-account/">My Account</a></li>
+<li class="wpmenucartli wpmenucart-display-right menu-item menu-item-type-post_type menu-item-object-page" id="wpmenucartli"><a class="wpmenucart-contents empty-wpmenucart-visible" href="http://www.templatespro.com.br/templates/" title="Start shopping"><i class="wpmenucart-icon-shopping-cart-0"></i><span class="cartcontents">0 items</span><span class="amount">&#36;&nbsp;0.00</span></a></li></ul></nav>    </div>
+    <aside id="woocommerce_product_categories-3" class="content widget woocommerce widget_product_categories"><h5 class="section-nav-title">Product Categories</h5><select name='product_cat' class='dropdown_product_cat'><option value=""  selected='selected'>Select a category</option>	<option class="level-0" value="html">HTML&nbsp;(3)</option>
+	<option class="level-0" value="opencart">Opencart&nbsp;(0)</option>
+	<option class="level-0" value="wordpress">WordPress&nbsp;(0)</option>
+</select></aside><aside id="woocommerce_widget_cart-3" class="content widget woocommerce widget_shopping_cart"><div class="widget_shopping_cart_content"></div></aside></div>		<div class="wrapper-site">
+
+		
+		<section class="header-site header-sticky header-overlay content header-left" >
+
+		    <div class="header-secondary content-small invert">
+                    <div class=" clearfix">
+                                                
+                                <nav class="pull-right"><ul id="menu-menu-login" class="nav nav-horizontal"><li id="menu-item-30" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-30"><a href="http://www.templatespro.com.br/my-account/">My Account</a></li>
+<li id="menu-item-64" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-64"><a href="http://www.templatespro.com.br/my-account/">Log In</a></li>
+</ul></nav>                            </div>
+            </div>
+						<div class=" header-block">
+				<div class="logo">
+	
+	<a href="http://www.templatespro.com.br/" class="custom-logo-link" rel="home" itemprop="url"><img width="100" height="100" src="http://www.templatespro.com.br/wp-content/uploads/2015/05/logo_p.png" class="custom-logo" alt="" itemprop="logo" /></a>		<div class="site-description">
+						<h3 class="sitename sitetitle"><a href="http://www.templatespro.com.br">Templates PRO</a></h3>
+			<p class="tagline"></p>
+					</div>
+	</div>
+
+<nav class="nav nav-horizontal">
+    
+    <ul id="menu-menu-top-1" class="menu"><li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-home menu-item-25"><a href="http://www.templatespro.com.br/">Home</a></li>
+<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-29"><a href="http://www.templatespro.com.br/templates/">Templates</a></li>
+<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-100"><a href="http://www.templatespro.com.br/contact/">Contact</a></li>
+<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-88"><a href="http://www.templatespro.com.br/my-account/">My Account</a></li>
+<li class="wpmenucartli wpmenucart-display-right menu-item menu-item-type-post_type menu-item-object-page" id="wpmenucartli"><a class="wpmenucart-contents empty-wpmenucart-visible" href="http://www.templatespro.com.br/templates/" title="Start shopping"><i class="wpmenucart-icon-shopping-cart-0"></i><span class="cartcontents">0 items</span><span class="amount">&#36;&nbsp;0.00</span></a></li></ul>
+    
+    <a href="" class="responsive-nav"  data-toggle="#off-canvas-right" data-toggle-class="open">
+	<span class="l-menu"></span>
+</a>
+</nav>			</div>
+					</section>
+
+		
+		<section id="wrapper-content" class="wrapper-content">
+<div class="post content-main clearfix container">
+    <div class="grid">
+
+        
+        <article class="column span-12">
+            <header class="section-title large">
+			<h1 class="heading">No posts found</h1>
+	</header>
+<div class="story">
+	<p>Use the search form below to find the page you're looking for:</p>
+	<form role="search" method="get" class="search-form" action="http://www.templatespro.com.br/">
+				<label>
+					<span class="screen-reader-text">Search for:</span>
+					<input type="search" class="search-field" placeholder="Search &hellip;" value="" name="s" />
+				</label>
+				<input type="submit" class="search-submit" value="Search" />
+			</form></div>        </article>
+
+            </div>
+</div>
+
+
+			<div id="back-to-top">
+				<a href="#top">Back to top</a>
+			</div> <!-- back-to-top -->
+
+		</section>
+
+		
+		<section id="footer" class="footer-site invert">
+						<div class=" content clearfix">
+				
+				
+								<div class="grid copyright">
+											<div class="column span-6 span-10">
+							<p class="site-text">© 2014 - 2016 - Templates PRO. All rights reserved.</p>
+						</div>
+										<div class="column span-6 span-2 clearfix t-right">
+						                        <a class="btn-facebook" href="https://www.facebook.com/TemplatesPRO" target="_blank"><img class="img-responsive" src="http://www.templatespro.com.br/wp-content/uploads/2015/05/icn_facebook.png" alt="Facebook"></a>
+					</div>
+				</div>
+							</div>
+			
+					</section><!-- END / FOOTER -->
+		
+	</div><!-- END / MAIN SITE #wrapper -->
+		<script type='text/javascript'>
+/* <![CDATA[ */
+var wpmenucart_ajax = {"ajaxurl":"http:\/\/www.templatespro.com.br\/wp-admin\/admin-ajax.php","nonce":"0f1c21d4cc"};
+/* ]]> */
+</script>
+<script type='text/javascript' src='http://www.templatespro.com.br/wp-content/plugins/woocommerce-menu-bar-cart/javascript/wpmenucart.js?ver=2.5.8'></script>
+<script type='text/javascript'>
+/* <![CDATA[ */
+var wc_add_to_cart_params = {"ajax_url":"\/wp-admin\/admin-ajax.php","wc_ajax_url":"\/mos7\/google_analytics_auto.js?wc-ajax=%%endpoint%%","i18n_view_cart":"View Cart","cart_url":"http:\/\/www.templatespro.com.br\/cart\/","is_cart":"","cart_redirect_after_add":"yes"};
+/* ]]> */
+</script>
+<script type='text/javascript' src='//www.templatespro.com.br/wp-content/plugins/woocommerce/assets/js/frontend/add-to-cart.min.js?ver=2.6.11'></script>
+<script type='text/javascript' src='//www.templatespro.com.br/wp-content/plugins/woocommerce/assets/js/jquery-blockui/jquery.blockUI.min.js?ver=2.70'></script>
+<script type='text/javascript'>
+/* <![CDATA[ */
+var woocommerce_params = {"ajax_url":"\/wp-admin\/admin-ajax.php","wc_ajax_url":"\/mos7\/google_analytics_auto.js?wc-ajax=%%endpoint%%"};
+/* ]]> */
+</script>
+<script type='text/javascript' src='//www.templatespro.com.br/wp-content/plugins/woocommerce/assets/js/frontend/woocommerce.min.js?ver=2.6.11'></script>
+<script type='text/javascript' src='//www.templatespro.com.br/wp-content/plugins/woocommerce/assets/js/jquery-cookie/jquery.cookie.min.js?ver=1.4.1'></script>
+<script type='text/javascript'>
+/* <![CDATA[ */
+var wc_cart_fragments_params = {"ajax_url":"\/wp-admin\/admin-ajax.php","wc_ajax_url":"\/mos7\/google_analytics_auto.js?wc-ajax=%%endpoint%%","fragment_name":"wc_fragments"};
+/* ]]> */
+</script>
+<script type='text/javascript' src='//www.templatespro.com.br/wp-content/plugins/woocommerce/assets/js/frontend/cart-fragments.min.js?ver=2.6.11'></script>
+<script type='text/javascript'>
+/* <![CDATA[ */
+var layers_script_settings = {"header_sticky_breakpoint":"270"};
+/* ]]> */
+</script>
+<script type='text/javascript' src='http://www.templatespro.com.br/wp-content/themes/layerswp/assets/js/layers.framework.js?ver=1.5.1'></script>
+<script type='text/javascript' src='http://www.templatespro.com.br/wp-includes/js/wp-embed.min.js?ver=44d3a00f90623f88e9a1706f7fc201f6'></script>
+<!-- WooCommerce JavaScript -->
+<script type="text/javascript">
+jQuery(function($) { 
+
+				jQuery( '.dropdown_product_cat' ).change( function() {
+					if ( jQuery(this).val() != '' ) {
+						var this_page = '';
+						var home_url  = 'http://www.templatespro.com.br/';
+						if ( home_url.indexOf( '?' ) > 0 ) {
+							this_page = home_url + '&product_cat=' + jQuery(this).val();
+						} else {
+							this_page = home_url + '?product_cat=' + jQuery(this).val();
+						}
+						location.href = this_page;
+					}
+				});
+			
+ });
+</script>
+</body>
+</html>
